46746d9d3ef677083dcfeac5e64bb485694f6d84
qchenwhy006 <57992497+qchenwhy006@users.noreply.github.com>
2020-01-14 10:43:19 +0100

    From jupyter notebook

--- Build_Vocab.py
+++ Build_Vocab.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+
 
 
 import numpy as np
@@ -15,7 +15,7 @@ import tensorflow.python.framework.d
 
 # ## 1. 加载数据
 
-# In[4]:
+
 
 
 train_data = pd.read_csv("AutoMaster_Trainset.csv")
@@ -35,7 +35,7 @@ print(test_data.info())
 
 # ## 3. 去掉缺失值
 
-# In[39]:
+
 
 
 train_data = train_data.dropna(axis=0)
@@ -43,7 +43,7 @@ test_data = test_data.dropna(axis=0)
 
 # ## 4. 合并dataframe、预处理标点符号、切分句子
 
-# In[183]:
+
 
 
 train_data_report = train_data[['Report']]
@@ -71,7 +71,7 @@ def cut(f, freq=50):
 words_dialogue = cut(' '.join(frame['Dialogue']))
 words_question = cut(' '.join(frame['Question']))
 
-# In[184]:
+
 
 
 # 考虑report列
@@ -79,7 +79,7 @@ words_report = cut(' '.join(train_data['Report']))
 
 # ## 5. 去掉数字、字母、空字符
 
-# In[185]:
+
 
 
 ls = '[a-zA-Z0-9’"#$%&\'()*+,-./:;<=>…“”‘’！[\\]^_`{|}~]+'
@@ -103,7 +103,7 @@ words_report2 = null_remove(words_report1)
 
 # ## 6. 停用词采样
 
-# In[186]:
+
 
 
 ## 前两列选出unique的词语加入字典
@@ -116,14 +116,14 @@ print(total_count)
 # ((set(words_dialogue2).union(set(words_question2))). union(set(words_report2))).union(car_set)
 
 
-# In[187]:
+
 
 
 vocab_initial = list(set(total_list))
 vocab_count = len(vocab_initial)
 print(vocab_count)
 
-# In[193]:
+
 
 
 vocab_to_int = {word: index for index, word in enumerate(vocab_initial)}  # 7797
@@ -132,15 +132,13 @@ int_words = [vocab_to_int[w] for w in total_list]  # index 12714
 print(len(int_words))
 print(len(list(set(int_words))))
 
-# In[189]:
+
 
 
 ##所以vocab_initial中无停用词的删除
 vocab_final = vocab_initial
 
 # ## 7. 输出文件
-
-# In[191]:
 
 
 # 先创建并打开一个文本文件
