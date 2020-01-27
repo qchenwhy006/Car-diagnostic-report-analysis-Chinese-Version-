96000204f2b425c7f8cfc08db814599f028765de
chenqianqian <qchenwhy006@gmail.com>
2020-01-27 17:22:47 +0100

    version1

--- Build_Vocab.py
+++ Build_Vocab.py
@@ -1,156 +1,95 @@
-#!/usr/bin/env python
-# coding: utf-8
+# encoding=utf-8
 
-
-
-
-import numpy as np
+import jieba
 import pandas as pd
-import tensorflow as tf
-import string
 import re
 from collections import Counter
-import jieba
-import tensorflow.python.framework.d
 
-# ## 1. 加载数据
-
-
-
-
-train_data = pd.read_csv("AutoMaster_Trainset.csv")
-test_data = pd.read_csv("AutoMaster_TestSet.csv")
-
-# ## 2. 查看数据集结构
-
-# In[5]:
+#输入输出文件信息
+path1='C:\\Users\\qianqian\\Desktop\\AutoMaster_TrainSet.txt'
+path2='C:\\Users\\qianqian\\Desktop\\AutoMaster_TestSet.txt'
+path_user_dict='C:\\Users\\qianqian\\Desktop\\user_dict.txt'
+path_stop_words='C:\\Users\\qianqian\\Desktop\\chineseStopword.txt'
 
 
-display(train_data.head())
-display(test_data.head())
-print(train_data.shape)
-print(test_data.shape)
-print(train_data.info())
-print(test_data.info())
-
-# ## 3. 去掉缺失值
+#读取文件，返回文件的dataframe格式
+def load_dataset(train_data_path,test_data_path):
+    train_data=pd.read_csv(train_data_path)
+    test_data=pd.read_csv(test_data_path)
+    return train_data,test_data
 
 
+#对文件中的标点进行过滤删除
+def clean__sentence(sentence):
+    if isinstance(sentence,str):
+        return re.sub(r'[\s+\-\|\!\/\[\]\{\}_,，,\【\】！？：]',' ',sentence)
+    else:
+        return ' '
 
 
-train_data = train_data.dropna(axis=0)
-test_data = test_data.dropna(axis=0)
-
-# ## 4. 合并dataframe、预处理标点符号、切分句子
-
-
-
-
-train_data_report = train_data[['Report']]
-train_data_main = train_data.loc[:, 'Brand':'Dialogue']
-test_data_main = test_data.loc[:, 'Brand':]
-frame = pd.concat([train_data_main, test_data_main])
-
-# 加上中文符号
-punc_str = '[’!"#$%&\'()*+,-./:;<=>?@，。?★、…【】：。，、《》（）～？“”‘’！％——[\\]^_`{|}~]+...'
+#对文件中的停用词进行过滤删除
+def filter_stopwords(stop_word_path,words):
+    file=open(stop_word_path,'r',encoding='utf-8')
+    stop_words=file.readlines()
+    stop_words=[stop_word.strip() for stop_word in stop_words]
+    return [word for word in words if word not in stop_words]
 
 
-def cut(f, freq=50):
-    f = re.sub('\[\S*?\]', ' ', f)
+#分词
+def cut(f, freq=5):
     cut_result = jieba.cut(f, cut_all=False)
     sr = ' '.join(cut_result)
-    sr_after = re.sub(punc_str, ' ', sr)
-
-    word_counts = Counter(sr_after.split(' '))
+    word_counts = Counter(sr.split(' '))
 
     # 考虑词频
-    trimmed_words = [word for word in word_counts if word_counts[word] > freq]
+    trimmed_words = [str(word) for word in word_counts if word_counts[word] > freq]
     return trimmed_words
 
 
-words_dialogue = cut(' '.join(frame['Dialogue']))
-words_question = cut(' '.join(frame['Question']))
-
-
-
-
-# 考虑report列
-words_report = cut(' '.join(train_data['Report']))
-
-# ## 5. 去掉数字、字母、空字符
-
-
-
-
-ls = '[a-zA-Z0-9’"#$%&\'()*+,-./:;<=>…“”‘’！[\\]^_`{|}~]+'
-
-# 去掉字母数字
-words_dialogue1 = [re.sub(ls, '', i) for i in words_dialogue]
-words_question1 = [re.sub(ls, '', i) for i in words_question]
-words_report1 = [re.sub(ls, '', i) for i in words_report]
-
-
-# 去掉空字符
-def null_remove(wordslist):
-    while '' in wordslist:
-        wordslist.remove('')
-    return wordslist
-
-
-words_dialogue2 = null_remove(words_dialogue1)
-words_question2 = null_remove(words_question1)
-words_report2 = null_remove(words_report1)
-
-# ## 6. 停用词采样
-
-
-
-
-## 前两列选出unique的词语加入字典
-s1 = set(frame['Brand'])
-s2 = set(frame['Model'])
-car_set = s1.union(s2)
-total_list = list(car_set) + words_dialogue2 + words_question2 + words_report2
-total_count = len(total_list)
-print(total_count)
-# ((set(words_dialogue2).union(set(words_question2))). union(set(words_report2))).union(car_set)
 
 
 
 
 
-vocab_initial = list(set(total_list))
-vocab_count = len(vocab_initial)
-print(vocab_count)
+
+
+# 主函数
+if __name__ == "__main__":
+    # 读取文件
+    train_df, test_df = load_dataset(path1, path2)
+    train_df = train_df.fillna(' ')
+    test_df = test_df.fillna(' ')
+    print('train data size: {}, test data size: {}'.format(len(train_df), len(test_df)))
+
+    #合并文件,形成句子
+    train_df=pd.concat([train_df['Question'],train_df['Dialogue'],train_df['Report']],axis=0,join='outer', ignore_index=True)
+    test_df=pd.concat([test_df['Question'],test_df['Dialogue']],axis=0,join='outer', ignore_index=True)
+    df=pd.concat([train_df,test_df],join='outer',axis=0, ignore_index=True)
+
+
+
+    # 使用正则过滤
+    for i in range(len(df)):
+        df[i] = clean__sentence(df[i])
+
+
+    #切词
+    jieba.load_userdict("C:\\Users\\qianqian\\Desktop\\user_dict.txt")
+    words=cut(' '.join(df))
 
 
 
 
-vocab_to_int = {word: index for index, word in enumerate(vocab_initial)}  # 7797
-int_to_vocab = {index: word for index, word in enumerate(vocab_initial)}
-int_words = [vocab_to_int[w] for w in total_list]  # index 12714
-print(len(int_words))
-print(len(list(set(int_words))))
+    #形成vocab
+    vocab_initial = list(set(words))
+    print('filter words: {}'.format(len(vocab_initial)))
+    vocab_to_int = {word: index for index, word in enumerate(vocab_initial)}
+    int_to_vocab = {index: word for index, word in enumerate(vocab_initial)}
 
 
 
-
-##所以vocab_initial中无停用词的删除
-vocab_final = vocab_initial
-
-# ## 7. 输出文件
-
-
-# 先创建并打开一个文本文件
-file = open('user_vocab.txt', 'w')
-
-# 遍历字典的元素，将每项元素的key和value分拆组成字符串，注意添加分隔符和换行符
-for k, v in vocab_to_int.items():
-    file.write(str(k) + ' ' + str(v) + '\n')
-
-# 注意关闭文件
-file.close()
-
-# for k,v in sorted(dict_temp.items()):
-# 	file.write(str(k)+' '+str(v)+'\n')
-# file.close()
+    # 将结果保存到文件中
+    file = open('vocab_CQQ.txt', 'w',encoding='utf-8')
+    for k, v in vocab_to_int.items():
+        file.write(str(k) + ':' + str(v) + '\n')
+    file.close()
